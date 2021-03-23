a27f99cebc0e4c251b2e3c2365d79f27fe87ac2e
tbsdvl <tristonburns97@gmail.com>
2021-03-23 16:57:38 -0700

    Adding Manager inquirer

---
+++ Develop/dist/htmlgen.js
@@ -0,0 +1,20 @@
+function generateHTML() {
+  const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Team Profile Generator</title>
+    </head>
+    <body>
+        <header>
+            <h1>My Team Profile Generator</h1>
+        </header>
+    </body>
+    </html>`
+
+    return html
+};
+
+module.exports = generateHTML();
--- Develop/index.js
+++ Develop/index.js
@@ -1,3 +1,30 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+
+inquirer.prompt([
+    {
+        type: "input",
+        message: "Enter Team Manager's Name: ",
+        name: "managerName",
+    },
+    {
+        type: "input",
+        message: "Enter Team Manager's ID: ",
+        name: "managerID",
+    },
+    {
+        type: "input",
+        message: "Enter Team Manager's Email: ",
+        name: "managerEmail",
+    },
+    {
+        type: "input",
+        message: "Enter Team Manager's Office Number: ",
+        name: "managerOffice",
+    }
+])
+.then((data) => {
+    console.log(data);
+})
